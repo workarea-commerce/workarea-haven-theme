fdee6bb42bf12a4b44d2c69605ce96570cebd7b6
Jake Beresford <jberesford@weblinc.com>
2018-02-21 12:40:23 -0800

    * Update SVG icon component to use font-size as base unit
    HAVNTHEME-23

--- app/helpers/workarea/storefront/style_guide_icons_helper.rb
+++ app/helpers/workarea/storefront/style_guide_icons_helper.rb
@@ -1,6 +1,7 @@
 module Workarea
   module Storefront
     module StyleGuideIconsHelper
+      # TODO - Remove this helper once ECOMMERCE-5446 is merged and released
       def style_guide_icons
         super + Dir["#{Theme.installed.root}/app/assets/images/workarea/**/icons/**/*.svg"].uniq.sort
       end
