fe4adc23bf5cc287c21b45c85f6dbede3f4d6408
Jake Beresford <jberesford@weblinc.com>
2018-02-23 15:02:46 -0800

    Logo and mobile header tweaks
    * Adds a new breakpoint for smedium devices
    * Add Logo
    * Add generic workarea favicon to storefront
    
    HAVNTHEME-14

---
+++ config/initializers/assets.rb
@@ -0,0 +1,1 @@
+Rails.application.config.assets.precompile += %w(workarea/storefront/favicon.ico)
--- config/initializers/theme.rb
+++ config/initializers/theme.rb
@@ -9,4 +9,22 @@ Workarea.configure do |config|
     primary_font_family: "poppins",
     secondary_font_family: "pt_serif"
   }
+
+  config.storefront_break_points = {
+    small: 320,
+    small_horizontal: 456,
+    medium: 760,
+    wide: 960,
+    x_wide: 1160
+  }
+
+  config.admin_break_points = {
+    small: 320,
+    small_horizontal: 456,
+    medium: 760,
+    wide: 960,
+    x_wide: 1160
+  }
+
+  config.favicon_path = "workarea/storefront/favicon.ico"
 end
